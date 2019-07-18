95aa197cf02e0eca65ccd145a7d9c7d34fc5cfb0
Tempuss <ben3787@gmail.com>
2019-07-19 01:48:54 +0900

    Add Struct & print Packet Info

--- format.h
+++ format.h
@@ -6,21 +6,22 @@
 /*
  * @brief Ethernet Header Format Length
  */
-#define ETH_PREAM = 7;
-#define ETH_SFD = 1;
-#define ETH_ADDR_LENGTH = 6;
-#define ETH_TAG = 4;
-#define ETH_LENGTH = 2;
-#define ETH_DATA_MIN_LENGTH = 46;
-#define ETH_DATA_MAX_LENGTH = 1500;
-#define ETH_CRC = 4;
+#define ETH_PREAM 7
+#define ETH_SFD 1
+#define ETH_ADDR_LENGTH 0x6
+#define ETH_TAG 0x4
+#define ETH_LENGTH 0x2
+#define ETH_DATA_MIN_LENGTH 0x46
+#define ETH_DATA_MAX_LENGTH 0x1500
+#define ETH_CRC 0x4
 
 /*
  * @brief IP Header Format Length
  */
-#define IP_VERSION = 4;
-#define IP_LENGTH = 4;
-#define IP_TYPE = 8;
+#define IP_VERSION 1
+#define IP_LENGTH 1
+#define IP_TYPE 1;
+#define IP_TOS 8
 #define IP_TOTAL_LENGTH = 16;
 #define IP_IDENTI = 16;
 #define IP_FLAG = 3;
@@ -30,7 +31,7 @@
 #define IP_HEAD_CHECK = 16;
 #define IP_BOOK = 6;
 #define IP_FLAG_BIT = 1;
-#define IP_ADDRLENGTH = 32;
+#define IP_ADDRLENGTH 32;
 #define IP_CHECK = 16;
 #define IP_URGENT = 16;
 
@@ -46,6 +47,18 @@
 #define TCP_WINDOW = 16;
 #define TCP_DATA_MIN = 0;
 #define TCP_DATA_MAX = 10;
+
+/*
+struct eth_header {
+    u_int8_t dst_mac[ETH_ADDR_LENGTH];	//Destination Mac Address
+    u_int8_t src_mac[ETH_ADDR_LENGTH];	// Source Mac Address
+    u_int16_t type; // Protocol Type
+};
+*/
+
+
+
+
 
 
 #endif // FORMAT_H
--- main.cpp
+++ main.cpp
@@ -1,17 +1,76 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <arpa/inet.h>
 #include <format.h>
+#include <typeinfo>
+
+#pragma pack(push,1)
+struct eth_header {
+    u_int8_t dst_mac[ETH_ADDR_LENGTH];	//Destination Mac Address
+    u_int8_t src_mac[ETH_ADDR_LENGTH];	// Source Mac Address
+    u_int16_t type; // Protocol Type
+};
+struct ip_header {
+    uint8_t version;
+    uint8_t tos;
+    uint16_t ip_len;
+    uint16_t id;
+    uint16_t ip_off;
+    uint8_t ttl;
+    uint8_t protocol_id;
+    uint16_t header_check;
+    in_addr src_ip;
+    in_addr dst_ip;
+    /*
+    u_int32_t src_ip;
+    u_int32_t dst_ip;
+    */
+};
+
+struct tcp_header {
+    uint16_t src_port;
+    uint16_t dst_port;
+    uint32_t seq;
+    uint32_t ack;
+    uint16_t flags;
+    uint16_t window_size;
+    uint16_t checksum;
+    uint16_t urg_pointer;
+    uint *option;
+};
+#pragma pack(pop);
 
 void usage() {
   printf("syntax: pcap_test <interface>\n");
   printf("sample: pcap_test wlan0\n");
 }
 
+
+
+void printHex(int length, const u_char* packet ) {
+    for (int i =0;i<length;i++) {
+
+
+        printf("%02x ", packet[i]);
+
+
+        if(i%15==0) {
+            printf("\n");
+        }
+    }
+
+    printf("\n");
+}
+
+
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     usage();
     return -1;
   }
+
 
   char* dev = argv[1];
   char errbuf[PCAP_ERRBUF_SIZE];
@@ -23,11 +82,73 @@ int main(int argc, char* argv[]) {
 
   while (true) {
     struct pcap_pkthdr* header;
+    const eth_header* ethdr;
+    const ip_header* ipdr;
+    const tcp_header* tcpdr;
     const u_char* packet;
     int res = pcap_next_ex(handle, &header, &packet);
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
+
+
+    ethdr = reinterpret_cast<const eth_header*>(packet);
+
+    printf("\n");
+    /*
+    printf("DST MAC :");
+    for(int i=0;i<ETH_ADDR_LENGTH;i++) {
+        printf("%02x ", ethdr->dst_mac[i]);
+    }
+    printf("\n");
+
+    printf("DST MAC :");
+    for(int i=0;i<ETH_ADDR_LENGTH;i++) {
+        printf("%02x ", ethdr->src_mac[i]);
+    }
+    printf("\n");
+
+    printf("ETHERNET TYPE :");
+    printf("%02x", ethdr->type);
+    printf("\n");
+    */
+
+    //ipdr = reinterpret_cast<const ip_header*>(packet);
+    ipdr = reinterpret_cast<const ip_header*>(packet+sizeof(struct eth_header));
+
+
+    printf("\n");
+    /*
+    printf("VER : %02x\n", ipdr->version);
+    printf("tos : %02x\n", ipdr->tos);
+    printf("Packet Length : %d\n", ipdr->ip_len);
+    printf("Packet Length : %u\n", ipdr->ip_len);
+    printf("Packet Length : %x\n", ipdr->ip_len);
+    printf("id : %02x\n", ipdr->id);
+    printf("ttl : %02x\n", ipdr->ttl);
+    */
+    printf("src_ip : %s\n", inet_ntoa(ipdr->src_ip));
+    printf("dst_ip : %s\n", inet_ntoa(ipdr->dst_ip));
+    printf("\n");
+
+    tcpdr = reinterpret_cast<const tcp_header*>(packet+sizeof(struct eth_header)+sizeof(ip_header));
+    printf("src_port :%d\n", htons(tcpdr->src_port));
+    printf("dst_port :%d\n", htons(tcpdr->dst_port));
+    /*
+    printf("SRC IP :");
+    for(int j=0; j<IP_VERSION; j++)
+    {
+        printf("%d ", ipdr->version[j]);
+    }
+    */
+
+
+
     printf("%u bytes captured\n", header->caplen);
+    printHex(header->caplen, packet);
+
+    struct tm *req_time;
+    req_time = localtime(&header->ts.tv_sec);
+    printf( "%d-%d-%d %d:%d:%d\n" , req_time->tm_year + 1900 , req_time->tm_mon + 1 , req_time->tm_mday , req_time->tm_hour , req_time->tm_min , req_time->tm_sec );
   }
 
   pcap_close(handle);
