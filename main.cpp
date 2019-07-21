f5367aacc07d1b5237c5f015bffe896851e88ad6
Tempuss <ben3787@gmail.com>
2019-07-21 20:58:25 +0900

    Update Structure

--- main.cpp
+++ main.cpp
@@ -3,35 +3,25 @@
 #include <time.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <format.h>
 #include <typeinfo>
 #include <iostream>
 #include <sstream>
+#define ETH_ADDR_LENGTH 0x6
 
-#pragma pack(push,1)
-
-
-/*
-class eth_header2 {
-    uint8_t dst_mac[ETH_ADDR_LENGTH];
-    uint8_t src_mac[ETH_ADDR_LENGTH];
-    uint16_t type;
-
-    public:
-        eth_header2(const u_char*)
-        {
-
-        }
-
-};
-*/
-
+/**
+ * @brief The eth_header struct
+ * @detail Ethernet Header Format
+ */
 struct eth_header {
     u_int8_t dst_mac[ETH_ADDR_LENGTH];	//Destination Mac Address
     u_int8_t src_mac[ETH_ADDR_LENGTH];	// Source Mac Address
     u_int16_t type; // Protocol Type
 };
 
+/**
+ * @brief The ip_header struct
+ * @detail IP Header Format
+ */
 struct ip_header {
     uint8_t version;
     uint8_t tos;
@@ -45,6 +35,10 @@ struct ip_header {
     in_addr dst_ip;
 };
 
+/**
+ * @brief The tcp_header struct
+ * @detail TCP Header Format
+ */
 struct tcp_header {
     uint16_t src_port;
     uint16_t dst_port;
@@ -55,9 +49,7 @@ struct tcp_header {
     uint16_t window_size;
     uint16_t checksum;
     uint16_t urg_pointer;
-    uint *option;
 };
-#pragma pack(pop);
 
 void usage() {
   printf("syntax: pcap_test <interface>\n");
@@ -66,23 +58,17 @@ void usage() {
 
 
 
+/**
+ * @brief printHex
+ * @detail Print Packet Data with Hex Format
+ * @param length
+ * @param packet
+ */
 void printHex(int length, const u_char* packet ) {
-    /*
-    for (int i =0;i<length;i++) {
-
-
-        printf("%02x ", packet[i]);
-
-
-        if(i%15==0) {
-            printf("\n");
-        }
-    }
-    */
 
     int i=0;
     while(i<length) {
-        printf("%02x ", packet[i]);
+        printf("%02X ", packet[i]);
 
         ++i;
         if (i%8==0)
@@ -98,9 +84,15 @@ void printHex(int length, const u_char* packet ) {
     printf("\n");
 }
 
-bool ipCheck(u_int16_t type)
+/**
+ * @brief ipCheck
+ * @detail Check that IP Header Exists
+ * @param type
+ * @return
+ */
+bool headerCheck(int size)
 {
-    if (type == 8)
+    if (size == 20)
     {
         return true;
     }
@@ -108,14 +100,41 @@ bool ipCheck(u_int16_t type)
     return false;
 }
 
-bool tcpCheck(u_int8_t protocol)
-{
-    if (protocol == 6)
-    {
-        return true;
+/**
+ * @brief printValue
+ * @detail Print IP Format Data
+ * @param in_addr ip
+ */
+void printValue(in_addr ip) {
+    printf("%s:", inet_ntoa(ip));
+}
+
+/**
+ * @brief printValue
+ * @detail Print Int Format Data
+ * @param int port
+ */
+void printValue(int port) {
+    printf("%d ", port);
+}
+
+
+/**
+ * @brief printValue
+ * @detail print Mac Address
+ * @param uint8_t[] data
+ */
+void printValue(const uint8_t data[]) {
+    int size = sizeof(data);
+    for(int i=0;i<size;i++) {
+        printf("%02X", data[i]);
+        if (i != size-1)
+        {
+            printf(":");
+        }
     }
 
-    return false;
+    printf(" ");
 }
 
 
@@ -140,87 +159,66 @@ int main(int argc, char* argv[]) {
     const ip_header* ipdr;
     const tcp_header* tcpdr;
     const u_char* packet;
-    uint packet_size = 0;
+    const unsigned char *tcp_data;
+
+    int res = pcap_next_ex(handle, &header, &packet);
+    int packet_size = 0;
     int ip_size = 0;
     int tcp_data_size = 0;
-    int res = pcap_next_ex(handle, &header, &packet);
+    int tcp_size = 0;
+    int eth_size = 0;
+
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
 
 
-    ethdr = reinterpret_cast<const eth_header*>(packet);
-    if (ipCheck(ethdr->type) == false)
-    {
-        //printf("Not an IP HEader");
-   }
+    //Get Ethernet Header From Packet
+    ethdr = (eth_header*)(packet);
 
+    //Get IP Header From Packet
+    ipdr = (struct ip_header*)(packet+sizeof(struct eth_header));
 
-    printf("\n");
-
-    ipdr = reinterpret_cast<const ip_header*>(packet+sizeof(struct eth_header));
     packet_size = htons(ipdr->packet_len);
     ip_size = (ipdr->version&0x0F)*4;
 
-
-
-    if (tcpCheck(ipdr->protocol_id))
+    //Check Next IP Header Size
+    if (headerCheck(ip_size) == true)
     {
-        //printf("Not TCP");
+
+        //Get TCP Header From Packet
+        tcpdr = (struct tcp_header*)(packet+sizeof(struct eth_header)+sizeof(ip_header));
+
+        //Calculate TCP Header Size to Print TCP Data
+        tcp_size = ((tcpdr->header_len&0xF0)>>4)*4;
+
+        //Check Next Tcp Header
+        if (headerCheck(tcp_size) == true)
+        {
+            eth_size = sizeof(ethdr)*4;
+
+            //Printable TCP Data Size
+            tcp_data_size = packet_size - (ip_size + tcp_size);
+
+            printValue(ethdr->src_mac);
+            printValue(ethdr->dst_mac);
+
+            printValue(ipdr->src_ip);
+            printValue(tcpdr->src_port);
+
+            printValue(ipdr->dst_ip);
+            printValue(tcpdr->dst_port);
+
+            //Check TCP Data Exists
+
+            {
+                //Get TCP Data from Packet
+                tcp_data = (u_char*)(packet+sizeof(struct eth_header)+sizeof(ip_header)+ tcp_size);
+                printValue(tcp_data);
+            }
+        }
+        printf("\n");
     }
 
-
-
-
-    tcpdr = reinterpret_cast<const tcp_header*>(packet+sizeof(struct eth_header)+sizeof(ip_header));
-    int tcp_size = ((tcpdr->header_len&0xF0)>>4)*4;
-    int option_size = tcp_size - 20;
-    int eth_size = sizeof(ethdr)*4;
-
-    tcp_data_size = packet_size - (ip_size + tcp_size);
-
-
-    for(int i=0;i<ETH_ADDR_LENGTH;i++) {
-        printf("%02x", ethdr->src_mac[i]);
-        if (i != ETH_ADDR_LENGTH-1)
-        {
-            printf(":");
-        }
-    }
-    printf(" ");
-
-    for(int i=0;i<ETH_ADDR_LENGTH;i++) {
-        printf("%02x", ethdr->dst_mac[i]);
-        if (i != ETH_ADDR_LENGTH-1)
-        {
-            printf(":");
-        }
-    }
-    printf(" ");
-
-    printf(" %s:%d", inet_ntoa(ipdr->dst_ip), htons(tcpdr->dst_port));
-    printf(" ");
-    printf(" %s:%d", inet_ntoa(ipdr->src_ip), htons(tcpdr->src_port));
-    printf(" ");
-
-    if (tcp_data_size > 0)
-    {
-        const unsigned char *tcp_data = reinterpret_cast<const unsigned char*>(packet+sizeof(struct eth_header)+sizeof(ip_header)+ tcp_size);
-        if (tcp_data_size > 10)
-        {
-            tcp_data_size = 10;
-        }
-
-        for(int i=0;i<tcp_data_size;i++)
-        {
-            printf("%02x ", tcp_data[i]);
-        }
-    }
-    printf("\n");
-
-
-    struct tm *req_time;
-    req_time = localtime(&header->ts.tv_sec);
-    //printf( "%d-%d-%d %d:%d:%d\n" , req_time->tm_year + 1900 , req_time->tm_mon + 1 , req_time->tm_mday , req_time->tm_hour , req_time->tm_min , req_time->tm_sec );
   }
 
   pcap_close(handle);
